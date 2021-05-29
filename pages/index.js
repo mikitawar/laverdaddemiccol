6cac033c9ac215e5881918b5a5beaafbb805c444
mikitawar <85023126+mikitawar@users.noreply.github.com>
2021-05-29 12:56:26 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -45,6 +45,7 @@ export default function Home() {
 
       <footer className={styles.footer}>
       <a href="https://info.flagcounter.com/lvj6"><img src="https://s11.flagcounter.com/count2/lvj6/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_10/viewers_0/labels_0/pageviews_0/flags_0/percent_0/" alt="Flag Counter" border="0"/></a>
+      <a href="https://www.flagcounter.me/details/cjY"><img src="https://www.flagcounter.me/cjY/" alt="Flag Counter"/></a>
       </footer>
     </div>
   )
